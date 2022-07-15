75e4e324bb2064c939ab6943d3a67b97917a6e48
Frederique Retsema <fretsema@fretsema.nl>
2022-07-15 18:01:38 +0200

    first

--- docker/exec-strace.sh
+++ docker/exec-strace.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
+#!/bin/sh
 
 FULL_PATH_HTTPD=$(find / -name httpd)
 
-strace "${FULL_PATH_HTTPD}" -D FOREGROUND || exit 1
+strace "${FULL_PATH_HTTPD}" -D FOREGROUND 2>&1  | grep open | grep O_RDONLY | grep -v ENOENT | grep -v O_DIRECTORY | awk -F'"' '{print "COPY --from-dir=build "$2" "$2}' > /build_dir/docker-copy-commands-strace
--- e2e/cypress.config.js
+++ e2e/cypress.config.js
@@ -6,6 +6,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
     e2e: {
         baseUrl: 'http://{LOCALHOST}:8080',
+        specPattern: '/e2e/cypress/**/*.cy.js',
         supportFile: false,
         setupNodeEvents(on, config) {}
     }
