5bfe5ad5b911f84de0c1269b39bd6f3ef7a6b1ac
Frederique Retsema <fretsema@fretsema.nl>
2022-07-16 07:07:14 +0200

    first

--- docker/exec-strace.sh
+++ docker/exec-strace.sh
@@ -2,4 +2,4 @@
 
 FULL_PATH_HTTPD=$(find / -name httpd)
 
-strace "${FULL_PATH_HTTPD}" -D FOREGROUND 2>&1  | grep open | grep O_RDONLY | grep -v ENOENT | grep -v O_DIRECTORY | awk -F'"' '{print "COPY --from-dir=build "$2" "$2}' > /build_dir/docker-copy-commands-strace
+strace "${FULL_PATH_HTTPD}" -D FOREGROUND 2>&1  | grep open | grep O_RDONLY | grep -v ENOENT | grep -v O_DIRECTORY | awk -F'"' '{print "COPY --from=build "$2" "$2}' > /build_dir/docker-copy-commands-strace
